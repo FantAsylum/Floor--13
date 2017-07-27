e43f2e799a8b1778c773973aee0714034b7de043
patriot <patriot130690@gmail.com>
2017-07-27 23:28:30 +0300

    libgdx project created

---
+++ android/src/com/floor13/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.floor13.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.floor13.game.Floor--13;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Floor--13(), config);
+	}
+}
---
+++ desktop/src/com/floor13/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.floor13.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.floor13.game.Floor--13;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Floor--13(), config);
+	}
+}
