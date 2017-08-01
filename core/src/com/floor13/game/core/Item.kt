bbcc5990f02b3e4a9e4e9efb46337e8d604d0b5c
takahawk <takahawkkun@gmail.com>
2017-08-02 00:45:33 +0300

    Weapon class added

--- core/src/com/floor13/game/core/Item.kt
+++ core/src/com/floor13/game/core/Item.kt
@@ -1,6 +1,6 @@
 package com.floor13.game.core
 
-abstract class Item(
-        val weight: Int,
+interface Item {
+        val weight: Int
         val descriptionKey: String // we'll use keys for internationalization
-)
+}
---
+++ core/src/com/floor13/game/core/Weapon.kt
@@ -0,0 +1,13 @@
+package com.floor13.game.core
+
+class Weapon(
+        override val weight: Int,
+        override val descriptionKey: String,
+        val baseDamage: IntRange,
+        val damageType: DamageType,
+        // val ammo: AmmoType
+) : Item
+
+enum class DamageType {
+    MECHANICAL, ENERGETIC
+}
