e04adca6bc57fa78c40fdf5dd57187d84f19d93f
takahawk <takahawkkun@gmail.com>
2017-08-08 16:07:46 +0300

    implementing Screen changed with ScreenAdapter subclassing (for clarity)

--- core/src/com/floor13/game/screens/MainMenuScreen.kt
+++ core/src/com/floor13/game/screens/MainMenuScreen.kt
@@ -1,33 +1,5 @@
 package com.floor13.game.screens
 
-import com.badlogic.gdx.Screen
+import com.badlogic.gdx.ScreenAdapter
 
-class MainMenuScreen : Screen {
-    override fun hide() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun show() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun render(delta: Float) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun pause() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun resume() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun resize(width: Int, height: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun dispose() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-}
+class MainMenuScreen : ScreenAdapter() {}
--- core/src/com/floor13/game/screens/SplashScreen.kt
+++ core/src/com/floor13/game/screens/SplashScreen.kt
@@ -1,36 +1,5 @@
 package com.floor13.game.screens
 
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.Screen
-import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.ScreenAdapter
 
-class SplashScreen : Screen {
-    override fun hide() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun show() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun render(delta: Float) {
-        Gdx.gl.glClearColor(0f, 0f, 0f, 0f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-    }
-
-    override fun pause() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun resume() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun resize(width: Int, height: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun dispose() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-}
+class SplashScreen : ScreenAdapter() {}
