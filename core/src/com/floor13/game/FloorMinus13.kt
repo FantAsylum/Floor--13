11e0f328a0514af6f4b3508fd9d7aaa5132f6429
takahawk <takahawkkun@gmail.com>
2017-08-08 16:06:50 +0300

    Clear screen added in main game rendering routine (before rendering current screen)

--- core/src/com/floor13/game/FloorMinus13.kt
+++ core/src/com/floor13/game/FloorMinus13.kt
@@ -1,5 +1,6 @@
 package com.floor13.game
 import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
 import com.floor13.game.util.MyStack
@@ -15,6 +16,9 @@ class FloorMinus13(): ApplicationAdapter() {
     }
 
     override fun render () {
+        Gdx.gl.glClearColor(0f, 0f, 0f, 0f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
         screenStack.peek()?.render(Gdx.graphics.deltaTime)
     }
 
