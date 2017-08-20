66af46825958b90f64743e98b97de85f4cc487e1
takahawk <takahawkkun@gmail.com>
2017-08-20 10:51:12 +0300

    Asset manager added with method for getting texture

--- core/src/com/floor13/game/FloorMinus13.kt
+++ core/src/com/floor13/game/FloorMinus13.kt
@@ -1,13 +1,18 @@
 package com.floor13.game
+
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.assets.AssetManager
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
+
 import com.floor13.game.util.MyStack
 import com.floor13.game.screens.SplashScreen
 
 object FloorMinus13: ApplicationAdapter() {
 
+    private val assetManager = AssetManager()
     private val screenStack = MyStack<Screen>()
 
     override fun create() {
@@ -15,11 +20,19 @@ object FloorMinus13: ApplicationAdapter() {
         screenStack.push(splashScreen)
     }
 
-    override fun render () {
+    override fun render() {
         Gdx.gl.glClearColor(0f, 0f, 0f, 0f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
         screenStack.peek()?.render(Gdx.graphics.deltaTime)
+    }
+
+    override fun dispose() {
+        assetManager.dispose()
+    }
+
+    fun getTexture(atlas: String, name: String): TextureAtlas.AtlasRegion {
+        return assetManager.get<TextureAtlas>(atlas).findRegion(name)
     }
 
     fun removeScreen() {
