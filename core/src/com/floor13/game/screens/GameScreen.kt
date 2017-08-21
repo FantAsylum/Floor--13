10cb6f730379f45e11de189a0fc725e250663714
takahawk <takahawkkun@gmail.com>
2017-08-21 13:07:43 +0300

    Stages and world added to GameScreen and stub logic added to MenuScreen

--- core/src/com/floor13/game/screens/GameScreen.kt
+++ core/src/com/floor13/game/screens/GameScreen.kt
@@ -1,7 +1,17 @@
 package com.floor13.game.screens
 
 import com.badlogic.gdx.ScreenAdapter
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.utils.viewport.ExtendViewport
 
-class GameScreen : ScreenAdapter() {
-    // TODO: implementation
+import com.floor13.game.core.World
+
+class GameScreen(world: World) : ScreenAdapter() {
+    val levelStage = Stage(ExtendViewport(0f, 0f))
+    val hudStage = Stage() // TODO: pick viewport
+    
+    override fun render(delta: Float) {
+        levelStage.act(delta)
+        levelStage.draw()
+    }
 }
--- core/src/com/floor13/game/screens/MainMenuScreen.kt
+++ core/src/com/floor13/game/screens/MainMenuScreen.kt
@@ -3,15 +3,20 @@ package com.floor13.game.screens
 import com.badlogic.gdx.ScreenAdapter
 
 import com.floor13.game.FloorMinus13
+import com.floor13.game.core.World
+import com.floor13.game.core.map.Map
+import com.floor13.game.core.map.MapGenerator
+import com.floor13.game.core.map.EmptyMapGenerator
 
 class MainMenuScreen : ScreenAdapter() {
-
+    val generator = EmptyMapGenerator(10, 10) // FIXME: stub logic
+    
     init {
         // TODO: make UI setup here
     }
     
     override fun render(delta: Float) {
         // TODO: remove and replace with actual rendering logic
-        FloorMinus13.pushScreen(GameScreen())
+        FloorMinus13.pushScreen(GameScreen(World(generator.generate())))
     }
 }
