f4f414580ffe7ed7d869b5cb8a2fcd0cb84aa987
takahawk <takahawkkun@gmail.com>
2017-08-21 16:01:51 +0300

    Map actor added to screen

--- core/src/com/floor13/game/screens/GameScreen.kt
+++ core/src/com/floor13/game/screens/GameScreen.kt
@@ -5,10 +5,15 @@ import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.viewport.ExtendViewport
 
 import com.floor13.game.core.World
+import com.floor13.game.actors.MapActor
 
 class GameScreen(world: World) : ScreenAdapter() {
     val levelStage = Stage(ExtendViewport(0f, 0f))
     val hudStage = Stage() // TODO: pick viewport
+
+    init {
+        levelStage.addActor(MapActor(world.map))
+    }
     
     override fun render(delta: Float) {
         levelStage.act(delta)
