64485760b89788956e36ce733ef67c38de6a412e
takahawk <takahawkkun@gmail.com>
2017-09-07 16:07:22 +0300

    Added Position convenience methods

--- core/src/com/floor13/game/core/Position.kt
+++ core/src/com/floor13/game/core/Position.kt
@@ -1,3 +1,9 @@
 package com.floor13.game.core
 
-data class Position(val x: Int, val y: Int)
+data class Position(val x: Int, val y: Int) {
+	infix fun adjacentTo(that: Position) =
+		Math.abs(this.x - that.x) + Math.abs(this.y - that.y) == 1
+
+	fun withinBounds(width: Int, height: Int) =
+		x >= 0 && x < width && y >= 0 && y < height
+}
