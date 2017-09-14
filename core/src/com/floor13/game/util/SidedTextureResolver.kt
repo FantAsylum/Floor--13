d03439015603af874631c1a52ea116145efc3056
takahawk <takahawkkun@gmail.com>
2017-09-14 11:18:11 +0300

    Sided texture resolver refactored to be more generic

--- core/src/com/floor13/game/util/SidedTextureResolver.kt
+++ core/src/com/floor13/game/util/SidedTextureResolver.kt
@@ -6,6 +6,8 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion
 import ktx.collections.*
 
 import com.floor13.game.core.map.Map
+import com.floor13.game.core.map.width
+import com.floor13.game.core.map.height
 import com.floor13.game.core.map.Tile
 import com.floor13.game.core.map.Ground
 import com.floor13.game.core.map.Wall
@@ -16,29 +18,38 @@ import com.floor13.game.core.map.Door
  */
 class SidedTextureResolver(
         private val atlas: TextureAtlas,
-        private val map: Map
+		private val width: Int,
+		private val height: Int,
+		private val isSameTexture: (Int, Int, Int, Int) -> Boolean
 ) {
+
     private val cache: Array<Array<TextureRegion?>> = Array(
-            map.size,
-            { Array(map.getOrNull(0)?.size ?: 0, { null as TextureRegion? })}
+            width,
+            { Array(height, { null as TextureRegion? })}
     )
 
     private val variants = gdxArrayOf<TextureRegion>()
-    
+
+	constructor(atlas: TextureAtlas, map: Map)
+			: this(
+			atlas,
+			map.width,
+			map.height,
+			{ x1, y1, x2, y2 -> map[x1][y1] sameTileTypeAs map[x2][y2] }
+	)
+	
     fun getTexture(name: String, x: Int, y: Int): TextureRegion {
         return cache[x][y] ?: {
             val suffix = StringBuilder()
 
-            val tile = map[x][y]
-        
             // order convention: urld
-            if (y == (map.getOrNull(0)?.size ?: 0) - 1 || map[x][y + 1] sameTileTypeAs tile)
+            if (y == height - 1 || isSameTexture(x, y + 1, x, y))
                 suffix.append('u')
-            if (x == map.size - 1 || map[x + 1][y] sameTileTypeAs tile)
+            if (x == width - 1 || isSameTexture(x + 1, y, x, y))
                 suffix.append('r')
-            if (x == 0 || map[x - 1][y] sameTileTypeAs tile)
+            if (x == 0 || isSameTexture(x - 1, y, x, y))
                 suffix.append('l')
-            if (y == 0 || map[x][y - 1] sameTileTypeAs tile)
+            if (y == 0 || isSameTexture(x, y - 1, x, y))
                 suffix.append('d')
 
             val exactName = "${name}_${suffix}"
@@ -54,8 +65,11 @@ class SidedTextureResolver(
         }()
     }
 
-    private infix fun Tile.sameTileTypeAs(tile: Tile): Boolean {
-        // review this solution if you added additional Tile types
-        return this is Ground && tile is Ground || (!(this is Ground) && !(tile is Ground))
-    }
+	companion object {
+		private infix fun Tile.sameTileTypeAs(tile: Tile): Boolean {
+			// review this solution if you added additional Tile types
+			return this is Ground && tile is Ground || (!(this is Ground) && !(tile is Ground))
+		}
+	}
+
 }
